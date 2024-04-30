133e1d7df21b7c7b2bdd0a9707114e19d68c1879
Soyu Kim <mmx900@gmail.com>
2024-04-30 21:54:14 +0900

    Increasing timeout to prevent PendingConnectionError

--- spec/support/rspec.rb
+++ spec/support/rspec.rb
@@ -9,7 +9,7 @@ require_relative './locations'
 
 Capybara.javascript_driver = :cuprite
 Capybara.register_driver(:cuprite) do |app|
-  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800], browser_options: { 'no-sandbox': nil })
+  Capybara::Cuprite::Driver.new(app, window_size: [1200, 800], browser_options: { 'no-sandbox': nil }, timeout: 30)
 end
 
 WebMock.disable_net_connect!(allow_localhost: true)
